527414822f3107e950e784666f268c02b057c117
HypeR <antoine.mineau@gmail.com>
2012-03-09 03:41:38 +0100

    added database connector

---
+++ src/database/database.cpp
@@ -0,0 +1,79 @@
+#include "database.hpp"
+
+Database::Database() {}
+
+Database::~Database() {}
+
+void Database::connect()
+{
+  if (mysql_init(&mysql) == NULL)
+  {
+    std::cout << "Could'nt init a mysql connection." << std::endl;
+  }
+
+  if (!mysql_real_connect(&this->mysql, DB_HOST, DB_USER, DB_PASSWORD,
+                          DB_DATABASE, DB_PORT, DB_UNIX_SOCKET, DB_CLIENT_FLAG))
+  {
+    std::cout << "Couldn't connect to the database." << std::endl;
+    exit -1;
+  }
+
+}
+
+void Database::query(const char* query)
+{
+  MYSQL_RES* result = NULL;
+  MYSQL_ROW row = NULL;
+  unsigned int i = 0;
+  unsigned int num_fields = 0;
+  unsigned int error;
+
+  error = mysql_query(&this->mysql, query);
+  if (error != 0)
+  {
+    printf("Couldn't query the database : %d\n", error);
+  }
+  result = mysql_use_result(&this->mysql);
+  if (result)
+  {
+    num_fields = mysql_num_fields(result);
+    while ((row = mysql_fetch_row(result)))
+    {
+      for(i = 0; i < num_fields; i++)
+      {
+        printf("[%s] ",  row[i]);
+      }
+    }
+    mysql_free_result(result);
+  }
+  else
+  {
+    printf("No result found !\n");
+  }
+}
+
+void Database::close()
+{
+  if (&this->mysql != NULL)
+    mysql_close(&this->mysql);
+}
+
+void Database::queryById(const char *table, const char *fields, const int id)
+{
+  std::stringstream ss;
+  std::string query;
+
+  this->connect();
+  ss << "SELECT " << fields << " FROM "
+  << table << " WHERE id = " << id;
+  query = ss.str();
+  this->query(query.c_str());
+  this->close();
+}
+
+int main()
+{
+  Database db;
+  db.queryById("user", "user.login, user.password, user.last_login", 1);
+  return 0;
+}
---
+++ src/database/database.hpp
@@ -0,0 +1,55 @@
+// Database Implementation, Objects of the tables...
+
+//socket mysql_real_connect
+#include <winsock.h>
+//mysql methods
+#include <mysql.h>
+//cout
+#include <iostream>
+//stringstream
+#include <sstream>
+// exit
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef __DB_HPP__
+# define __DB_HPP__
+
+# define DB_HOST "localhost"
+# define DB_USER "admin"
+# define DB_PASSWORD "623ed0ede843b90fb79a862af71695d1"
+# define DB_DATABASE "batajelo"
+# define DB_PORT 0
+# define DB_UNIX_SOCKET NULL
+# define DB_CLIENT_FLAG 0
+
+class Database
+{
+public:
+  Database();
+  ~Database();
+  void queryById(const char*, const char*, const int);
+private:
+  MYSQL mysql;
+  void connect();
+  void query(const char*);
+  void close();
+};
+
+/*
+class User
+{
+public:
+    User(int id);
+    ~User();
+private:
+    std::string login;
+    std::string password;
+    std::string last_login;
+
+}
+*/
+#endif
+
+
+
