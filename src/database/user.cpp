ec4757b47726a257f7f6d08f5c90e1e9a6e8d817
Florent Le Coz <louiz@louiz.org>
2012-03-22 21:35:33 +0100

    Fix a little bit the get_friends method, but it still broken.

--- src/database/user.cpp
+++ src/database/user.cpp
@@ -6,35 +6,35 @@ User::User() {}
 
 User::~User() {}
 
-std::string User::get_class_name()
+const std::string User::get_class_name() const
 {
-	return "User";
+  return "User";
 }
 
 std::vector<User*> User::get_friends()
 {
-	std::vector<DbObject*>::iterator objs_it;
-	std::vector<User*> friends;
-	std::string where = "user.id IN (";
-	std::vector<DbObject*> friends_ids = Database::inst()->get_objects("user_friend.friend_id AS id", "user_friend", "user_id = " + this->get("id"));
-	if (friends_ids.size() > 0)
+  std::vector<DbObject*>::iterator objs_it;
+  std::vector<User*> friends;
+  std::string where = "user.id IN (";
+  std::vector<DbObject*> friends_ids = Database::inst()->get_objects("user_friend.friend_id AS id", "user_friend", "user_id = " + this->get("id"));
+  if (friends_ids.size() > 0)
+    {
+      for (objs_it = friends_ids.begin(); objs_it != friends_ids.end(); objs_it++)
 	{
-		for (objs_it = friends_ids.begin(); objs_it != friends_ids.end(); objs_it++)
-		{
-			where += (*objs_it)->get("id");
-			where += (objs_it != friends_ids.end() - 1) ? ", " : ")";
-		}
-		friends_ids.clear();
-		std::vector<DbObject*> friends_obj = Database::inst()->get_objects("user.id, user.login, user.password, user.last_login", "user", where);
-		if (friends_obj.size() > 0)
-		{
-			for (objs_it = friends_obj.begin(); objs_it != friends_obj.end(); objs_it++)
-				friends.push_back(static_cast<User*>(*objs_it));
-		}
-		else
-			log_warning("No friends Infos found !");
+	  where += (*objs_it)->get("id");
+	  where += (objs_it != friends_ids.end() - 1) ? ", " : ")";
 	}
-	else
-		log_warning("No friends found !");
-	return friends;
+      friends_ids.clear();
+      std::vector<DbObject*> friends_obj = Database::inst()->get_objects("user.id, user.login, user.password, user.last_login", "user", where);
+      if (friends_obj.size() > 0)
+	{
+	  for (objs_it = friends_obj.begin(); objs_it != friends_obj.end(); objs_it++)
+	    friends.push_back(static_cast<User*>(*objs_it));
+	}
+      else
+	log_warning("No friends Infos found !");
+    }
+  else
+    log_warning("No friends found !");
+  return friends;
 }
