b0250fe630a01b81fbb9f64950ace40a7c5f429c
dionisos <denis.baudouin@gmail.com>
2012-03-24 13:34:10 +0100

    add batajelo_client.cpp

---
+++ src/main/batajelo_client.cpp
@@ -0,0 +1,9 @@
+#include <istream>
+
+
+// batajelo_client main 
+int	main(void)
+{
+	cout<<"client"<<endl;
+	return EXIT_SUCCESS;
+}
---
+++ src/main/batajelo_server.cpp
@@ -0,0 +1,9 @@
+#include <istream>
+
+
+// batajelo_server main 
+int	main(void)
+{
+	cout<<"server"<<endl;
+	return EXIT_SUCCESS;
+}
