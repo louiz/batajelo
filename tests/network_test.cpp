c149ab2308e47a600842387e30424039395ee29e
Florent Le Coz <louiz@louiz.org>
2012-04-14 17:13:35 +0200

    Add a simple test for the network Command class.

---
+++ tests/network_test.cpp
@@ -0,0 +1,24 @@
+#include <config/config.hpp>
+#include <logging/logging.hpp>
+#include <network/command.hpp>
+
+#include <cstring>
+
+#define BOOST_TEST_MODULE network
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(network_suite1)
+
+BOOST_AUTO_TEST_CASE(command_consistency_test)
+{
+  Command* command = new Command;
+  command->set_body("coucou les amis");
+  command->set_name("FAKE_COMMAND");
+  command->pack();
+
+  BOOST_REQUIRE(command->header == std::string("FAKE_COMMAND.15:"));
+  BOOST_REQUIRE(command->body_size == 15);
+  BOOST_REQUIRE(strncmp(command->body, "coucou les amis", 15) == 0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
