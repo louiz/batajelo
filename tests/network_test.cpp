7aa93718d25e4fa81dbd3aab3471c82fd2ebeab0
Florent Le Coz <louiz@louiz.org>
2012-05-22 19:20:08 +0200

    Add new network tests to test more headers. #60

--- tests/network_test.cpp
+++ tests/network_test.cpp
@@ -9,7 +9,7 @@
 
 BOOST_AUTO_TEST_SUITE(network_suite1)
 
-BOOST_AUTO_TEST_CASE(command_consistency_test)
+BOOST_AUTO_TEST_CASE(command_consistency_test1)
 {
   Command* command = new Command;
   command->set_body("coucou les amis");
@@ -19,6 +19,48 @@ BOOST_AUTO_TEST_CASE(command_consistency_test)
   BOOST_REQUIRE(command->header == std::string("FAKE_COMMAND.15:"));
   BOOST_REQUIRE(command->body_size == 15);
   BOOST_REQUIRE(strncmp(command->body, "coucou les amis", 15) == 0);
+
+  delete command;
+}
+
+BOOST_AUTO_TEST_CASE(command_consistency_test2)
+{
+  Command* command = new Command;
+  command->set_body("");
+  command->set_name("G");
+  command->pack();
+
+  BOOST_REQUIRE(command->header == std::string("G:"));
+  BOOST_REQUIRE(command->body_size == 0);
+  BOOST_REQUIRE(strncmp(command->body, "", 0) == 0);
+
+  delete command;
+}
+
+BOOST_AUTO_TEST_CASE(command_consistency_test3)
+{
+  Command* command = new Command;
+  command->set_body("");
+  command->set_name("GUGU");
+  command->pack();
+
+  BOOST_REQUIRE(command->header == std::string("GUGU:"));
+  BOOST_REQUIRE(command->body_size == 0);
+  BOOST_REQUIRE(strncmp(command->body, "", 0) == 0);
+
+  delete command;
+}
+
+BOOST_AUTO_TEST_CASE(command_consistency_test4)
+{
+  Command* command = new Command;
+  command->set_body("salut");
+  command->set_name("G");
+  command->pack();
+
+  BOOST_REQUIRE(command->header == std::string("G5:"));
+  BOOST_REQUIRE(command->body_size == 5);
+  BOOST_REQUIRE(strncmp(command->body, "salut", 5) == 0);
 
   delete command;
 }
