1b3ccf0999b13810579f42d73b7da8aff5b1cfe1
HypeR <antoine.mineau@gmail.com>
2012-05-24 17:37:35 +0200

    removed code of game server and added code of server (handler for interfaces)

--- tests/server_main.cpp
+++ tests/server_main.cpp
@@ -5,33 +5,14 @@
 #include <network/remote_client.hpp>
 #include <network/game_server.hpp>
 #include <cstdlib>
-
+ 
 int main()
 {
-  if (!Config::read_conf("./batajelo.conf"))
-    return 1;
-  log_debug("Starting server");
-
-  unsigned long long int total = 0;
-  srand(getpid());
-  GameServer s(7879);
-  s.start();
-
-  Time time1 = boost::posix_time::microsec_clock::universal_time();
-  Time time2;
-  Duration dt;
-
-  while (true)
-    {
-      time2 = boost::posix_time::microsec_clock::universal_time();
-      dt += time2 - time1;
-      time1 = time2;
-      long i = get_number_of_updates(dt);
-      for (; i > 0; --i)
-  	{
-	  s.tick();
-  	}
-      s.poll(10);
-    }
-  return 0;
+if (!Config::read_conf("./batajelo.conf"))
+return 1;
+log_debug("Starting server");
+ 
+Server<RemoteClient> s(7878);
+s.start();
+s.run();
 }
