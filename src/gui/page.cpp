a2e67da504305fb14bf86003dd40681780882f12
HypeR <antoine.mineau@gmail.com>
2012-05-24 17:25:20 +0200

    Resolved indentations issue in page and added image variable in class

--- src/gui/page.cpp
+++ src/gui/page.cpp
@@ -1,3 +1,3 @@
-#include <ui/page.hpp>
+#include <gui/page.hpp>
 
 Page::Page(Ui* ui, sfg::Desktop* desktop, sf::RenderWindow* window): ui(ui), desktop(desktop), window(window) {}
--- src/gui/page.hpp
+++ src/gui/page.hpp
@@ -18,14 +18,20 @@ class Ui;
 class Page
 {
 public:
-	Page(Ui*, sfg::Desktop*, sf::RenderWindow*);
-	virtual void build_ui() = 0;
-	virtual void draw_background() = 0;
-	virtual void show() = 0;
-	virtual void hide() = 0;
-	sfg::Desktop* desktop;
-	sf::RenderWindow* window;
-	Ui* ui;
+  Page(Ui*, sfg::Desktop*, sf::RenderWindow*);
+  virtual void build_ui() = 0;
+  virtual void draw_background() = 0;
+  virtual void show() = 0;
+  virtual void hide() = 0;
+  virtual void activate() = 0;
+  virtual void deactivate() = 0;
+
+
+  sfg::Desktop* desktop;
+  sf::RenderWindow* window;
+  sf::Image img;
+  sf::Sprite sprite_bg;
+  Ui* ui;
 };
 
 #endif // __PAGE_HPP__
