222d4749d97be417474358882e9eb1695598c78b
HypeR <antoine.mineau@gmail.com>
2012-05-24 17:42:44 +0200

    Erased old gui_main, added new one

--- tests/gui_main.cpp
+++ tests/gui_main.cpp
@@ -1,31 +1,7 @@
-#include <gui/camera/camera.hpp>
-#include <world/world.hpp>
+#include <game/game.hpp>
 
-int main()
+int main(void)
 {
-  // Create the main window
-  sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(800, 600), "Menu coucou");
-
-  World* world = new World;
-  world->load_test();
-  Camera* camera = new Camera(world);
-
-  sf::Clock clock;
-
-  while (window->isOpen())
-    {
-      sf::Event event;
-      while (window->pollEvent(event))
-	{
-	  if (event.type == sf::Event::Closed)
-	    window->close();
-	  camera->handle_event(event);
-	}
-      sf::Time dt = clock.restart();
-      camera->update(dt);
-      window->clear();
-      camera->draw(window);
-      window->display();
-    }
-  return EXIT_SUCCESS;
+	Game game;
+	game.run();
 }
