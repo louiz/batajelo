7eadbe2b4c30fc6eda44a7b782784acabc29e5e2
Florent Le Coz <louiz@louiz.org>
2012-07-10 16:43:41 +0200

    Add missing mouse_selection files.

---
+++ src/gui/camera/mouse_selection.cpp
@@ -0,0 +1,69 @@
+#include <gui/camera/mouse_selection.hpp>
+
+MouseSelection::MouseSelection():
+  ongoing(false)
+{
+}
+
+MouseSelection::~MouseSelection()
+{
+}
+
+void MouseSelection::start(const sf::Vector2i& pos)
+{
+  // assert(ongoing == false);
+  this->start_pos = pos;
+  this->ongoing = true;
+}
+
+void MouseSelection::end()
+{
+  // assert(this->ongoing == true);
+  this->ongoing = false;
+}
+
+bool MouseSelection::contains(const sf::Vector2i& mouse_pos,
+                              const sf::Vector2u& pos,
+                              const uint width) const
+{
+  sf::Rect<uint> mouse_selection;
+  if (this->ongoing)
+    {
+      if ((this->start_pos.x < mouse_pos.x))
+        mouse_selection.left = this->start_pos.x;
+      else
+        mouse_selection.left = mouse_pos.x;
+      if ((this->start_pos.y < mouse_pos.y))
+        mouse_selection.top = this->start_pos.y;
+      else
+        mouse_selection.top = mouse_pos.y;
+      mouse_selection.top += width;
+      mouse_selection.left += width/2;
+      mouse_selection.width = ::abs(this->start_pos.x - mouse_pos.x);
+      mouse_selection.height = ::abs(this->start_pos.y - mouse_pos.y);
+    }
+  else
+    {
+      mouse_selection.left = mouse_pos.x + width/2;
+      mouse_selection.top = mouse_pos.y + width;
+      mouse_selection.width = 1;
+      mouse_selection.height = 1;
+    }
+  sf::Rect<uint> rectangle(pos.x, pos.y, width, width);
+  return rectangle.intersects(mouse_selection);
+  // if ((this->start_pos.x < mouse_pos.x))
+  //   {
+  //     if (((x - width/2) < this->start_pos.x) || ((x + width/2) > mouse_pos.x))
+  //       return false;
+  //   }
+  // else if (((x + width/2) > this->start_pos.x) || ((x - width/2) < mouse_pos.x))
+  //   return false;
+  // if ((this->start_pos.y < mouse_pos.y))
+  //   {
+  //     if (((y - width/2) < this->start_pos.y) || ((y + width/2) > mouse_pos.y))
+  //       return false;
+  //   }
+  // else if (((y + width/2) > this->start_pos.y) || ((y - width/2) < mouse_pos.y))
+  //   return false;
+  // return true;
+}
---
+++ src/gui/camera/mouse_selection.hpp
@@ -0,0 +1,43 @@
+/** @addtogroup Ui
+ *  @{
+ */
+
+/**
+ * A simple class defining a start position and some methods to know if an
+ * entity is inside the mouse selection or not.
+ * @class MouseSelection
+ */
+
+#ifndef __MOUSE_SELECTION_HPP__
+# define __MOUSE_SELECTION_HPP__
+
+#include <assert.h>
+#include <SFML/Graphics.hpp>
+
+class Camera;
+
+class MouseSelection
+{
+  friend class Camera;
+public:
+  MouseSelection();
+  ~MouseSelection();
+  void start(const sf::Vector2i&);
+  void end();
+  /**
+   * Returns whether or not the mouse selection contains that point.
+   */
+  bool contains(const sf::Vector2i&,
+                const sf::Vector2u&, const uint width = 0) const;
+private:
+  MouseSelection(const MouseSelection&);
+  MouseSelection& operator=(const MouseSelection&);
+  /**
+   * The position at which the mouse was pressed.
+   */
+  sf::Vector2i start_pos;
+  bool ongoing;
+};
+
+#endif // __MOUSE_SELECTION_HPP__
+
