b4d6e18b8d1163f914b975eb6361b8d37e3f81d4
HypeR <antoine.mineau@gmail.com>
2012-09-28 05:11:53 +0200

    updated test values

--- data/test.sql
+++ data/test.sql
@@ -1,4 +1,9 @@
 INSERT INTO achievement VALUES(1, 'first', 'EASY', 'desc'); 
 INSERT INTO map VALUES(1, 'shakuras plateau', 'map cool');
 INSERT INTO mode VALUES(1, '1v1');
-INSERT INTO replay VALUES(1, 1, 1, 'test');
+INSERT INTO map_mode VALUES(1, 1, 1);
+INSERT INTO race VALUES(1, "zerg", "RACE IMBA DE MERDE");
+INSERT INTO race VALUES(2, "protoss", "RACE ENCORE PLUS IMBA");
+INSERT INTO race VALUES(3, "terran", "LA RACE LA PLUS COOL DU MONDE !!!");
+INSERT INTO ladder VALUES(1, "bronze", "bronze league", 100);
+INSERT INTO season VALUES(1, NOW(), NOW());
