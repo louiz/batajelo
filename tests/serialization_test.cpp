6ec52fbce4b1e1755dee94199c28a858a5057440
Florent Le Coz <louiz@louiz.org>
2013-03-13 21:13:10 +0100

    Remove something actually not part of the test

--- tests/serialization_test.cpp
+++ tests/serialization_test.cpp
@@ -103,12 +103,6 @@ BOOST_AUTO_TEST_CASE(serialization_vec2_test4)
   BOOST_REQUIRE(c == b);
   BOOST_REQUIRE(c.x == b.x);
   BOOST_REQUIRE(c.y == b.y);
-
-  Vec2 meh;
-  meh.x = 234.4999f;
-  meh.y = 187.4999f;
-  log_error(meh.length());
-  log_error(meh.x.toLong());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
