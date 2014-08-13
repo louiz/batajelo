3a916283f2d808ec58e17508faf4c68c323ef4ae
Florent Le Coz <louiz@louiz.org>
2014-08-13 04:56:36 +0200

    Use google-test for two unittests

--- tests/config_test.cpp
+++ tests/config_test.cpp
@@ -1,48 +1,46 @@
 #include <config/config.hpp>
-#include <logging/logging.hpp>
 #include <iostream>
 #include <fstream>
 
-#define BOOST_TEST_MODULE config
-#include <boost/test/included/unit_test.hpp>
+#include <gtest/gtest.h>
 
-BOOST_AUTO_TEST_SUITE(config_suite1)
-
-static int a;
-
-void on_conf_changed()
+class ConfigTest: public testing::Test
 {
-  ++a;
-}
+protected:
 
-BOOST_AUTO_TEST_CASE(config_test1)
+  void SetUp()
+  {
+    std::ofstream os("tests/test.conf");
+    os << "  coucou=machin" << std::endl;
+    os << "number=  2" << std::endl;
+    os << "empty  =" << std::endl;
+    os.close();
+
+    a = 0;
+  }
+
+  int a;
+};
+
+
+TEST_F(ConfigTest, basic)
 {
-  std::ofstream os("tests/test.conf");
-  os << "  coucou=machin" << std::endl;
-  os << "number=  2" << std::endl;
-  os << "empty  =" << std::endl;
-  os.close();
-
-  a = 0;
-
-  BOOST_REQUIRE(Config::read_conf("tests/test.conf") == true);
-  Config::connect(&on_conf_changed);
-  BOOST_REQUIRE(Config::get("coucou", "") == "machin");
-  BOOST_REQUIRE(Config::get("does not exist", "zizi") == "zizi");
-  BOOST_REQUIRE(Config::get_int("number", 0) == 2);
-  BOOST_REQUIRE(Config::get_int("does not exist", 1) == 1);
+  EXPECT_EQ(Config::read_conf("tests/test.conf"), true);
+  Config::connect([&]() {++a;});
+  EXPECT_EQ(Config::get("coucou", ""), "machin");
+  EXPECT_EQ(Config::get("does not exist", "zizi"), "zizi");
+  EXPECT_EQ(Config::get_int("number", 0), 2);
+  EXPECT_EQ(Config::get_int("does not exist", 1), 1);
   Config::set("coucou", "coucou");
-  BOOST_REQUIRE(Config::get("coucou", "") == "coucou");
+  EXPECT_EQ(Config::get("coucou", ""), "coucou");
   Config::set("new", "hello");
-  BOOST_REQUIRE(Config::get("new", "") == "hello");
+  EXPECT_EQ(Config::get("new", ""), "hello");
   Config::set_int("number", 12, true);
-  BOOST_REQUIRE(Config::get_int("number", 0) == 12);
+  EXPECT_EQ(Config::get_int("number", 0), 12);
 
-  BOOST_REQUIRE(a == 1);
+  EXPECT_EQ(a, 1);
   Config::close(true);
-  BOOST_REQUIRE(Config::read_conf("tests/test.conf") == true);
-  BOOST_REQUIRE(Config::get_int("number", 0) == 12);
+  EXPECT_EQ(Config::read_conf("tests/test.conf"), true);
+  EXPECT_EQ(Config::get_int("number", 0), 12);
   Config::close(true);
 }
-
-BOOST_AUTO_TEST_SUITE_END()
--- tests/utils_test.cpp
+++ tests/utils_test.cpp
@@ -3,81 +3,73 @@
 #include <utils/scopeguard.hpp>
 #include <utils/zlib.hpp>
 
-#include <iostream>
-#include <config/config.hpp>
-#include <logging/logging.hpp>
+#include <gtest/gtest.h>
 
-#define BOOST_TEST_MODULE utils
-#include <boost/test/included/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(utils_suite)
-
-BOOST_AUTO_TEST_CASE(base64_test)
+TEST(Base64, Both)
 {
   std::string hello = "Coucou ici";
-  BOOST_REQUIRE(base64_decode(
-                  base64_encode(
-                      reinterpret_cast<const unsigned char*>(hello.c_str()), hello.size())) == "Coucou ici");
-
-  std::string encoded("TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=");
-  BOOST_REQUIRE(base64_decode(encoded) == "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.");
+  EXPECT_EQ(base64_decode(base64_encode(reinterpret_cast<const unsigned char*>(hello.c_str()), hello.size())),
+               "Coucou ici");
 }
 
-BOOST_AUTO_TEST_CASE(zlib_test)
+TEST(Base64, encode)
+{
+  std::string encoded("TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=");
+  EXPECT_EQ(base64_decode(encoded), "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.");
+}
+
+TEST(Zlib, decompressAndCompress)
 {
   std::string original("coucou coucou");
-  BOOST_REQUIRE(zlib_decompress(zlib_compress(original)) == "coucou coucou");
+  EXPECT_EQ(zlib_decompress(zlib_compress(original)), "coucou coucou");
 }
 
-BOOST_AUTO_TEST_CASE(trim_test)
+TEST(Strings, trim)
 {
   std::string original("    coucou\n");
   utils::trim(original);
-  BOOST_REQUIRE(original == "coucou");
+  EXPECT_EQ(original, "coucou");
 
   original = "    coucou";
   utils::trim(original);
-  BOOST_REQUIRE(original == "coucou");
+  EXPECT_EQ(original, "coucou");
 
   original = "coucou  ";
   utils::trim(original);
-  BOOST_REQUIRE(original == "coucou");
+  EXPECT_EQ(original, "coucou");
 
   original = "coucou";
   utils::trim(original);
-  BOOST_REQUIRE(original == "coucou");
+  EXPECT_EQ(original, "coucou");
 
   original = "\n\ncoucou \r\n ";
   utils::trim(original);
-  BOOST_REQUIRE(original == "coucou");
-
+  EXPECT_EQ(original, "coucou");
 }
 
-BOOST_AUTO_TEST_CASE(scopeguard_test)
+TEST(Scopeguard, everything)
 {
   int i = 5;
   {
-    BOOST_REQUIRE(i == 5);
+    ASSERT_EQ(i, 5);
     utils::ScopeGuard guard([&i]() {--i;});
-    BOOST_REQUIRE(i == 5);
+    ASSERT_EQ(i, 5);
   }
-  BOOST_REQUIRE(i == 4);
+  ASSERT_EQ(i, 4);
   {
-    BOOST_REQUIRE(i == 4);
+    ASSERT_EQ(i, 4);
     utils::ScopeGuard guard;
     guard.add_callback([&i]() {--i;});
     guard.add_callback([&i]() {--i;});
-    BOOST_REQUIRE(i == 4);
+    ASSERT_EQ(i, 4);
   }
-  BOOST_REQUIRE(i == 2);
+  ASSERT_EQ(i, 2);
   {
-    BOOST_REQUIRE(i == 2);
+    ASSERT_EQ(i, 2);
     utils::ScopeGuard guard;
     guard.add_callback([&i]() {--i;});
-    BOOST_REQUIRE(i == 2);
+    ASSERT_EQ(i, 2);
     guard.disable();
   }
-  BOOST_REQUIRE(i == 2);
+  ASSERT_EQ(i, 2);
 }
-
-BOOST_AUTO_TEST_SUITE_END()
