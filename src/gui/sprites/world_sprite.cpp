a50ad77ded06f55492fa2343a1b7b545883ffe56
Florent Le Coz <louiz@louiz.org>
2014-12-04 02:37:39 +0100

    Add a missing file

---
+++ src/gui/sprites/world_sprite.cpp
@@ -0,0 +1,5 @@
+#include <gui/sprites/world_sprite.hpp>
+
+const EnergyBar WorldSprite::standard_health_bar = {sf::Color::Green, sf::Color::Red,
+                                                    {64, 5},
+                                                    10, 100};
