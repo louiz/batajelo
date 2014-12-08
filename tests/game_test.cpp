cc8ce2dfff10a303b266ba0d6da03951eacec286
Florent Le Coz <louiz@louiz.org>
2014-12-08 18:49:44 +0100

    Rewrite game_test for gtest

--- tests/game_test.cpp
+++ tests/game_test.cpp
@@ -1,111 +1,117 @@
-#include <config/config.hpp>
-#include <logging/logging.hpp>
-#include <functional>
 #include <game/turn_handler.hpp>
-#include <game/turn.hpp>
-#include <game/action.hpp>
-#include <game/event.hpp>
 
-#define BOOST_TEST_MODULE game
-#include <boost/test/included/unit_test.hpp>
+#include <gtest/gtest.h>
 
-BOOST_AUTO_TEST_SUITE(turn_handler_suite1)
-
-static int global_a = 0;
-
-void coucou(Event* event)
-{
-  (void)event;
-  global_a++;
-}
-
-BOOST_AUTO_TEST_CASE(turn_handlerstd::placeholders::_1)
-{
-  global_a = 0;
-
-  TurnHandler t;
-  Action* a = new Action(std::bind(&coucou, std::placeholders::_1), new Event, 1);
-  BOOST_REQUIRE(t.insert_action(a, 0) == false);
-
-  Action* a2 = new Action(std::bind(&coucou, std::placeholders::_1), new Event, 0);
-  BOOST_REQUIRE(t.insert_action(a2, 1) == true);
-
-  Action* a3 = new Action(std::bind(&coucou, std::placeholders::_1), new Event, 0);
-  BOOST_REQUIRE(t.insert_action(a3, 2) == true);
-
-  Action* a4 = new Action(std::bind(&coucou, std::placeholders::_1), new Event, 0);
-  BOOST_REQUIRE(t.insert_action(a4, 1) == true);
-
-  for (int i = 0; i < 100; ++i)
-    t.tick();
-
-  Action* a5 = new Action(std::bind(&coucou, std::placeholders::_1), new Event, 0);
-  BOOST_REQUIRE(t.insert_action(a5, 14) == true);
-
-  Action* a6 = new Action(std::bind(&coucou, std::placeholders::_1), new Event, 0);
-  BOOST_REQUIRE(t.insert_action(a6, 4) == false);
-
-  Action* a7 = new Action(std::bind(&coucou, std::placeholders::_1), new Event, 0);
-  BOOST_REQUIRE(t.insert_action(a7, 14) == true);
-
-  Action* a8 = new Action(std::bind(&coucou, std::placeholders::_1), new Event, 0);
-  BOOST_REQUIRE(t.insert_action(a8, 14) == true);
-
-  Action* a9 = new Action(std::bind(&coucou, std::placeholders::_1), new Event, 0);
-  BOOST_REQUIRE(t.insert_action(a9, 15) == true);
-
-  for (int i = 0; i < 100; ++i)
-    t.tick();
-
-  BOOST_REQUIRE(global_a == 7);
-}
-
-BOOST_AUTO_TEST_CASE(turn_handler_2)
-{
-  global_a = 0;
-
-  TurnHandler t;
-
-  Action* a = new Action(std::bind(&coucou, std::placeholders::_1), new Event, 2);
-  BOOST_REQUIRE(t.insert_action(a, 1) == true);
-  Action* a2 = new Action(std::bind(&coucou, std::placeholders::_1), new Event, 2);
-  BOOST_REQUIRE(t.insert_action(a2, 2) == true);
-
-  // the action is not yet validated.
-  for (int i = 0; i < 1; ++i)
-    t.tick();
-  BOOST_REQUIRE(global_a == 0);
-
-  a->validate(1);
-  a->validate(2);
-  a2->validate(2);
-  for (int i = 0; i < 200; ++i)
-    t.tick();
-  BOOST_REQUIRE(global_a == 1);
-
-  a2->validate(1);
-  for (int i = 0; i < 200; ++i)
-    t.tick();
-  BOOST_REQUIRE(global_a == 2);
-}
-
-BOOST_AUTO_TEST_CASE(turn_handler_3)
+TEST(TurnHandler, Creation)
 {
   TurnHandler t;
-
-  Turn* turn;
-  turn = t.get_turn(1);
-  BOOST_REQUIRE(turn == 0);
-
-  Action* a = new Action(std::bind(&coucou, std::placeholders::_1), new Event, 2);
-  BOOST_REQUIRE(t.insert_action(a, 1) == true);
-  Action* a2 = new Action(std::bind(&coucou, std::placeholders::_1), new Event, 2);
-  BOOST_REQUIRE(t.insert_action(a2, 0) == false);
-
-  turn = t.get_turn(1);
-  BOOST_REQUIRE(turn != 0);
-  turn = t.get_turn(2);
-  BOOST_REQUIRE(turn == 0);
+  ASSERT_EQ(t.get_current_turn(), 0);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+TEST(Turn, Move)
+{
+  Turn t;
+  Turn t2 = std::move(t);
+  ASSERT_EQ(t2.is_ready(), false);
+  t2.mark_ready();
+  t = std::move(t);
+  ASSERT_EQ(t2.is_ready(), true);
+}
+
+TEST(TurnHandler, BasicAdvancement)
+{
+  TurnHandler t;
+  ASSERT_EQ(t.get_current_turn(), 0);
+
+  // The turn 1 is not ready, so even if we tick enough, we never go after
+  // the turn 0
+  for (auto i = 0u; i < TURN_TICKS; ++i)
+    t.tick();
+  ASSERT_EQ(0, t.get_current_turn());
+  for (auto i = 0u; i < TURN_TICKS; ++i)
+    t.tick();
+  ASSERT_EQ(0, t.get_current_turn());
+
+  t.mark_turn_as_ready();
+  // Now the turn is ready, we can advance to the next turn just with one
+  // tick
+  t.tick();
+  ASSERT_EQ(1, t.get_current_turn());
+
+  // Advance 4 turn (but fail because turn 2 is not ready)
+  for (auto i = 0u; i < TURN_TICKS*4; ++i)
+    t.tick();
+  ASSERT_EQ(1, t.get_current_turn());
+
+  // Mark turns up to turn 5 as ready
+  t.mark_turn_as_ready();
+  t.mark_turn_as_ready();
+  t.mark_turn_as_ready();
+  t.mark_turn_as_ready();
+
+  // Advance to turn 2
+  t.tick();
+  ASSERT_EQ(2, t.get_current_turn());
+
+  // Advance one turn and a little bit more
+  for (auto i = 0u; i < TURN_TICKS + 2; ++i)
+    t.tick();
+  ASSERT_EQ(3, t.get_current_turn());
+}
+
+TEST(TurnHandler, ExecuteActionOnTurnAdvancement)
+{
+  auto val = 0;
+
+  TurnHandler t;
+  ASSERT_EQ(t.get_current_turn(), 0);
+
+  // Insert an action to be executed on turn 2
+  t.insert_action([&val](){ val++; }, 2);
+
+  // Insert two actions to be executed on turn 3
+  t.insert_action([&val](){ val++; }, 3);
+  t.insert_action([&val](){ val++; }, 3);
+
+  // Mark turns up to turn 5 as ready
+  for (auto i = 0u; i < 5; ++i)
+    t.mark_turn_as_ready();
+
+  ASSERT_EQ(0, val);
+  // Go to turn 1, does not execute any action
+  for (auto i = 0u; i < TURN_TICKS; ++i)
+    t.tick();
+  ASSERT_EQ(0, val);
+
+  // Action on turn 2 is executed
+  for (auto i = 0u; i < TURN_TICKS; ++i)
+    t.tick();
+  ASSERT_EQ(1, val);
+
+  for (auto i = 0u; i < TURN_TICKS; ++i)
+    t.tick();
+  ASSERT_EQ(3, val);
+
+}
+
+TEST(TurnHandler, ActionCannotBeAddedInReadyTurn)
+{
+  TurnHandler t;
+  // mark turn 1 as ready
+  t.mark_turn_as_ready();
+  // Try to insert an action in turn 1
+  ASSERT_DEATH(t.insert_action([](){}, 1), "");
+}
+
+TEST(TurnHandler, NextTurnCallback)
+{
+  TurnHandler t;
+  auto turn = t.get_current_turn();
+  ASSERT_EQ(0, turn);
+  t.set_next_turn_callback([&turn](const TurnNb nb) { turn = nb; });
+  t.mark_turn_as_ready();
+  t.mark_turn_as_ready();
+  for (auto i = 0u; i < TURN_TICKS*2; ++i)
+    t.tick();
+  ASSERT_EQ(2, turn);
+}
