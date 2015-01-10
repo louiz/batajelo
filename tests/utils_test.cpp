3b66654114e79b35e6c75793d66b772c18d4c78d
Florent Le Coz <louiz@louiz.org>
2015-01-10 17:13:18 +0100

    Rewrite time.hpp to use std::chrono, and add tests
    ref #2268

--- tests/utils_test.cpp
+++ tests/utils_test.cpp
@@ -2,6 +2,7 @@
 #include <utils/string.hpp>
 #include <utils/scopeguard.hpp>
 #include <utils/zlib.hpp>
+#include <utils/time.hpp>
 
 #include <gtest/gtest.h>
 
@@ -72,4 +73,57 @@ TEST(Scopeguard,)
     guard.disable();
   }
   ASSERT_EQ(i, 2);
+}
+
+TEST(Time, BasicTick)
+{
+  using namespace std::chrono_literals;
+
+  utils::Duration dt = 1s;
+  auto ticks = utils::get_number_of_ticks(dt);
+  ASSERT_EQ(ticks, 100);
+  ASSERT_EQ(dt, 0s);
+}
+
+TEST(Time, BasicTick2)
+{
+  using namespace std::chrono_literals;
+
+  utils::Duration dt = 380000us;
+  auto ticks = utils::get_number_of_ticks(dt);
+  ASSERT_EQ(ticks, 38);
+  ASSERT_EQ(dt, 0s);
+}
+
+TEST(Time, Ticks)
+{
+  using namespace std::chrono_literals;
+
+  utils::Duration dt = 385600us;
+  auto ticks = utils::get_number_of_ticks(dt);
+  ASSERT_EQ(ticks, 38);
+  ASSERT_EQ(dt, 5600us);
+}
+
+TEST(Time, ConvertToFloatingSeconds)
+{
+  using namespace std::chrono_literals;
+
+  utils::Duration dt = 8420000us;
+  auto secs = utils::sec(dt);
+  ASSERT_EQ(secs, 8.42s);
+
+  dt = -8420000us;
+  secs = utils::sec(dt);
+  ASSERT_EQ(secs, -8.42s);
+}
+
+TEST(Time, NullTime)
+{
+  using namespace std::chrono_literals;
+
+  utils::Duration dt = 0h;
+  auto ticks = utils::get_number_of_ticks(dt);
+  ASSERT_EQ(ticks, 0);
+  ASSERT_EQ(dt, 0us);
 }
